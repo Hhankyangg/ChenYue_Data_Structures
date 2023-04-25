d2a8295eafb41abf24a77820a044b43debc2e0fc
Hank <hankyang428@gmail.com>
2023-04-25 19:48:13 +0800

    update hw1

---
+++ Hw/Hw1/hw1_二分查找(BinarySearch).c
@@ -0,0 +1,16 @@
+Position BinarySearch( List L, ElementType X ) {
+    int leftnum = 1;
+    int rightnum = L->Last;
+    while(leftnum <= rightnum) {
+        if (X < L->Data[(leftnum+rightnum)/2]) {
+            rightnum = (leftnum+rightnum)/2 - 1;
+        }
+        else if (X > L->Data[(leftnum+rightnum)/2]) {
+            leftnum = (leftnum+rightnum)/2 + 1;
+        }
+        else if (X == L->Data[(leftnum+rightnum)/2]) {
+            return (leftnum+rightnum)/2;
+        }
+    } 
+    return NotFound;
+}
